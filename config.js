cfe6cf488fca44c7b59956c44709d6e1ae3e3ae3
WHITEDEWIL99 <148486936+WHITEDEWIL99@users.noreply.github.com>
2024-02-01 22:25:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['94786728789', 'WHITE', true], 
+  ['94786728789', 'Abraham Dwamena', true],
+  ['94786728789', '𝙸𝚃𝚉 𝙼𝙴 𝚂𝙰𝙷𝙰𝙽', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  ['94786728789', 'Abraham Dwamena2', true],
+  ['94786728789', 'Abraham Dwamena3', true],
   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
+global.mods = ['94786728789', '94789481495', '923184474176','233531910067','233208179431'] 
+global.prems = ['94786728789','233208179431','94789481495', '918360234087','233531910067']
+global.allowed = ['94786728789', '233208179431', '94789481495', '918360234087','233531910067']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -55,10 +55,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '𝚀𝚄𝙴𝙴𝙽-𝙷𝙴𝙻𝙰𝙽𝙺𝙰 𝙱𝙾𝚃'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = '𝚀𝚄𝙴𝙴𝙽-𝙷𝙴𝙻𝙰𝙽𝙺𝙰 𝙱𝚈 𝙸𝚃𝚉 𝙼𝙴 𝚂𝙰𝙷𝙰𝙽' 
+global.author = '@𝙼𝚁 𝚂𝙸𝚂𝚂𝙾𝚁' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
